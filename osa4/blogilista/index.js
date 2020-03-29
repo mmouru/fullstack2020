02c3bf47101a7fdd1734bf23b5fb85717d42b18c
mmouru <marttimourujarvi@gmail.com>
2020-03-30 01:11:41 +0300

    step 2 done

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,14 +1,9 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./App')
 
-app.use(cors())
-app.use(express.json())
-app.use('/', blogsRouter)
+const server = http.createServer(app)
 
 const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
